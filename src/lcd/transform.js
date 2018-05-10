14dee45906509319c5e18f1fd8e693cfe30afbb5
jerome doucet <jerdct@gmail.com>
2018-05-10 19:54:19 +0200

    transform some numbers to their 'LCD-screen' representation

--- src/lcd/transform.js
+++ src/lcd/transform.js
@@ -1,4 +1,48 @@
+import {EOL} from 'os';
 
-export function transform() {
-  return "";
+const numberOnLcd = {
+  '0': {0: ' _ ', 1: '| |', 2: '|_|'},
+  '1': {0: '   ', 1: '  |', 2: '  |'},
+  '2': {0: ' _ ', 1: ' _|', 2: '|_ '},
+  '3': {0: ' _ ', 1: ' _|', 2: ' _|'},
+  '4': {0: '   ', 1: '|_|', 2: '  |'},
+  '5': {0: ' _ ', 1: '|_ ', 2: ' _|'},
+  '6': {0: ' _ ', 1: '|_ ', 2: '|_|'},
+  '7': {0: ' _ ', 1: '  |', 2: '  |'},
+  '8': {0: ' _ ', 1: '|_|', 2: '|_|'},
+  '9': {0: ' _ ', 1: '|_|', 2: '  |'}
+}
+
+export function transform(inputChars) {
+  let result = EOL;
+  for (let nbLine = 0; nbLine < 3; nbLine++) {
+    result += buildOneLine(inputChars, nbLine);
+    if (nbLine != 2) {
+      result += EOL;
+    }
+  }
+  return result;
+}
+
+function buildOneLine(inputChars, lineNumber) {
+  let line = ""
+  for (let charIndex = 0; charIndex < inputChars.length; charIndex++) {
+    line += tranformOneChar(inputChars.charAt(charIndex), lineNumber);
+    if (isNotLastCharOfLine(inputChars, charIndex)) {
+      line += "   ";
+    }
+  }
+  return line;
+}
+
+function tranformOneChar(inputChar, lineNumber) {
+  if (numberOnLcd[inputChar]) {
+    return numberOnLcd[inputChar][lineNumber];
+  } else {
+    return "   ";
+  }
+}
+
+function isNotLastCharOfLine(inputChars, charIndex) {
+  return charIndex != inputChars.length -1
 }
--- test/lcd/lcd_spec.js
+++ test/lcd/lcd_spec.js
@@ -4,9 +4,9 @@ import { transform } from '../../src/lcd'
 describe('LCD', () => {
 
   describe('transform', () => {
-   it('transform some numeric in order to print it directly on a LCD screen', () => {
+   it('should transform some numeric in order to print it directly on a LCD screen', () => {
     // given
-    const inputData = "012456789";
+    const inputData = "0123456789";
     const expectedResult = `
  _           _     _           _     _     _     _     _ 
 | |     |    _|    _|   |_|   |_    |_      |   |_|   |_|
@@ -14,7 +14,21 @@ describe('LCD', () => {
 
     // when
     const result = transform(inputData);
-    
+
+    // then
+    expect(result).to.eql(expectedResult); 
+   }); 
+   it('should replace non numeric chars by spaces', () => {
+    // given
+    const inputData = "0123.456 78a9";
+    const expectedResult = `
+ _           _     _                 _     _           _     _           _ 
+| |     |    _|    _|         |_|   |_    |_            |   |_|         |_|
+|_|     |   |_     _|           |    _|   |_|           |   |_|           |`
+
+    // when
+    const result = transform(inputData);
+
     // then
     expect(result).to.eql(expectedResult); 
    }); 
